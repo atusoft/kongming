f02bfe991b2f7369c0a35c8befe4f839ad192701
atusoft <atu0830@gmail.com>
2019-02-19 21:12:17 +0800

    do not need temp image file

--- gui.py
+++ gui.py
@@ -13,25 +13,26 @@ def load_katong_pic():
     s=re.search('src="([^"]*)"',img)
     download_img(s.group(1))
 
-def download_img(image_link):
-    import shutil
+def download_img(url):
+    from PIL import Image
     import requests
-    r=requests.get(image_link,stream=True)
-    with open('img.png','wb') as out_file:
-        shutil.copyfileobj(r.raw,out_file)
-    pic.image='img.png'
+    from io import BytesIO
+
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    pic.image=img
     pic.tk.place(x=0,y=0,relwidth=1,relheight=1)
-    del r
 
 def show_timer():
-    from time import gmtime, strftime
-    text.value=strftime(u"%y年%m月%d日 %H:%M:%S", gmtime())
+    from time import localtime, strftime
+    text.value=strftime(u"%y年%m月%d日 %H点%M", localtime())
+    load_katong_pic()
 
 
 app = App(width=420,height=340,bg="black")
 app.tk.attributes("-fullscreen",True)
 pic=Picture(app)
-pic.repeat(30000,load_katong_pic)
 text = Text(app,size=20,color="white",align='bottom')
-text.repeat(1000,show_timer)
+text.repeat(60000,show_timer)
+show_timer()
 app.display()
