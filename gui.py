472518e62e1567ca1c8a78c2a01c3bd466e026c5
atusoft <atu0830@gmail.com>
2019-02-21 07:54:13 +0800

    refine picture display

--- gui.py
+++ gui.py
@@ -1,6 +1,21 @@
 #! /usr/bin/python3
 from guizero import *
 
+import threading
+
+class MyThread(threading.Thread):
+    def __init__(self, func, *args):
+        super().__init__()
+
+        self.func = func
+        self.args = args
+
+        self.setDaemon(True)
+        self.start()    # 在这里开始
+
+    def run(self):
+        self.func(*self.args)
+
 def load_katong_pic():
     import feedparser
     import re
@@ -20,19 +35,21 @@ def download_img(url):
 
     response = requests.get(url)
     img = Image.open(BytesIO(response.content))
+    img.thumbnail((480,320),Image.ANTIALIAS) 
     pic.image=img
     pic.tk.place(x=0,y=0,relwidth=1,relheight=1)
 
 def show_timer():
     from time import localtime, strftime
-    text.value=strftime(u"%y年%m月%d日 %H点%M", localtime())
-    load_katong_pic()
+    text.value=strftime(u"%y-%m-%d %H:%M", localtime())
+    import subprocess
+    subprocess.call(['xdotool','keydown','Shift_L','keyup','Shift_L'])
 
-
-app = App(width=420,height=340,bg="black")
+app = App(width=480,height=320,bg="black")
 app.tk.attributes("-fullscreen",True)
 pic=Picture(app)
-text = Text(app,size=20,color="white",align='bottom')
+pic.repeat(60000,lambda :MyThread(load_katong_pic))
+text = Text(app,size=20,color="grey",align='bottom',font='Spaceport One')
 text.repeat(60000,show_timer)
 show_timer()
 app.display()
